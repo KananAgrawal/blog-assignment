babe72b6d38e08d2e265c423f9e1e6de4845a0ac
Kanan Agrawal <104626837+KananAgrawal@users.noreply.github.com>
2024-07-12 23:08:03 +0530

    Update settings.py

--- project1/settings.py
+++ project1/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'project1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'app1/Templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
